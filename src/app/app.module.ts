1389515cb110c9d812a5262541041ae1357ce909
ghailen <ghailenemark@gmail.com>
2022-03-01 12:07:08 +0100

    myapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ServerComponent} from "./server/server.component";
+import {HttpClientModule} from "@angular/common/http";
+import { ServersComponent } from './servers/servers.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
